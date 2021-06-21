81bf79095d2c7d12f2af6bb207c9cd1f4f52fda5
Shogo Akiyama <sakiyama@knox.edu>
2021-06-21 09:19:56 -0700

    Solve Luck Balance

---
+++ src/luckBalance/Solution.java
@@ -0,0 +1,81 @@
+package luckBalance;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'luckBalance' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. 2D_INTEGER_ARRAY contests
+     */
+
+	public static int luckBalance(int k, List<List<Integer>> contests) {
+		// Write your code here
+		Collections.sort(contests, (List<Integer> l1, List<Integer> l2) -> {
+			if (l1.get(1) == l2.get(1)) {
+				return l2.get(0) - l1.get(0);
+			}
+			return l1.get(1) - l2.get(1);
+		});
+
+		int luck = 0;
+
+		for (List<Integer> contest : contests) {
+			if (k - contest.get(1) >= 0) {
+				k -= contest.get(1);
+				luck += contest.get(0);
+			} else {
+				luck -= contest.get(0);
+			}
+		}
+
+		return luck;
+	}
+
+}
+
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+		String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+		int n = Integer.parseInt(firstMultipleInput[0]);
+
+		int k = Integer.parseInt(firstMultipleInput[1]);
+
+		List<List<Integer>> contests = new ArrayList<>();
+
+		IntStream.range(0, n).forEach(i -> {
+			try {
+				contests.add(Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+						.map(Integer::parseInt).collect(toList()));
+			} catch (IOException ex) {
+				throw new RuntimeException(ex);
+			}
+		});
+
+		int result = Result.luckBalance(k, contests);
+
+		bufferedWriter.write(String.valueOf(result));
+		bufferedWriter.newLine();
+
+		bufferedReader.close();
+		bufferedWriter.close();
+	}
+}
